55e3b324cbc5139dde46494ec5a062d96750eb6b
Dennis Schipper <dennis@designfordetails.com>
2025-10-07 12:33:37 +0100

    Message list animations

--- packages/weaviate-agent-chat-ui/src/components/Message/Message.tsx
+++ packages/weaviate-agent-chat-ui/src/components/Message/Message.tsx
@@ -4,6 +4,8 @@ import { IMessage, IMessageSuggestion } from "../../types"
 import { MessageContent } from "./MessageContent/MessageContent"
 import { MessageSuggestions } from "./MessageSuggestions/MessageSuggestions"
 import { IMessageSuggestionsTitleProps } from "./MessageSuggestions/MessageSuggestionsTitle/MessageSuggestionsTitle"
+import { AnimatePresence, motion, MotionProps } from "motion/react"
+import useMeasure from "react-use-measure"
 
 export interface IMessageProps {
   message: IMessage
@@ -20,18 +22,35 @@ export const Message = (props: IMessageProps) => {
     console.log(message, suggestion)
   }
 
+  const [ ref, bounds ] = useMeasure()
+
+  const wrapperMotionProps: MotionProps = {
+    initial: { height: bounds.height },
+    animate: { height: bounds.height }
+  }
+
+  const motionProps: MotionProps = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+    transition: { delay: 0.33 }
+  } 
+  
   return (
-    <div className="message">
-      <MessageSource {...message.source} />
-      <div className="messageBody">
-        <MessageContent message={message}  />
-        <MessageInput display={displayMeta} onSubmit={onSubmitMeta} />
-      </div>
-      <MessageSuggestions 
-        onClickSuggestion={onClickSuggestion}
-        message={message} 
-        title={props.title}
-      />
-    </div>
+    <AnimatePresence>
+      <motion.div className="messageWrapper" {...wrapperMotionProps}>
+        <motion.div {...motionProps} className="message" ref={ref}>
+          <MessageSource {...message.source} />
+          <div className="messageBody">
+            <MessageContent message={message}  />
+            <MessageInput display={displayMeta} onSubmit={onSubmitMeta} />
+          </div>
+          <MessageSuggestions 
+            onClickSuggestion={onClickSuggestion}
+            message={message} 
+            title={props.title}
+          />
+        </motion.div>
+      </motion.div>
+    </AnimatePresence>
   )
 }
